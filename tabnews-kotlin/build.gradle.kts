955684c8e94e0847ce0d0f5120e45bd62846b3e7
Raphael <raphaelfc@gmail.com>
2025-11-30 19:42:30 -0300

    version bump

--- tabnews-kotlin/build.gradle.kts
+++ tabnews-kotlin/build.gradle.kts
@@ -40,7 +40,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "com.github.rphlfc"
             artifactId = "tabnews-kotlin"
-            version = "1.0.2"
+            version = "1.0.3"
 
             pom {
                 name.set("TabNews Kotlin")
